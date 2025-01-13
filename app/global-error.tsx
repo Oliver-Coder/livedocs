963ee044b53986db2d6074e8aeed16b4de8d0f69
Oliver-Coder <dienergrant@gmail.com>
2025-01-13 15:48:02 +0200

    Remove Sentry example page and improve Sentry integration.
    This commit deletes the unused Sentry example page and introduces enhancements to the Sentry setup in `next.config.mjs`, including source map optimizations, tunnel routing, and additional Sentry features. It also updates dependencies such as `@sentry/nextjs` and other related packages in `package-lock.json` to their latest versions, ensuring better compatibility and performance. Additionally, a `qodana.yaml` file is added for Qodana code analysis configuration.

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -2,18 +2,18 @@
 
 import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
-import { useEffect } from "react";
+import {useEffect} from "react";
 
 export default function GlobalError({ error }: { error: Error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    useEffect(() => {
+        Sentry.captureException(error);
+    }, [error]);
 
-  return (
-    <html>
-      <body>
+    return (
+        <html>
+        <body>
         {/* Your Error component here... */}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
